c9c056edbbb6c9dc3b109dc64a261081a5d7a86e
marcellandreas <marcellandreas.123@gmail.com>
2025-11-29 13:46:12 +0700

    chore: setup Vue 3 project with Vite, Tailwind, and shadcn-vue

---
+++ src/components/ui/button/Button.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Primitive } from "reka-ui";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from ".";
+
+const props = defineProps();
+</script>
+
+<template>
+  <Primitive
+    data-slot="button"
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), props.class)"
+  >
+    <slot />
+  </Primitive>
+</template>
